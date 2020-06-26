f77233f84e59a7dc5b840be0e2f7a4da6512b93e
Ricardo Angeles <Ricardo.ang.rmz@gmail.com>
2020-06-26 17:38:29 -0500

    Proyect-init

---
+++ src/js/routes/home/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GroupMenuItem, MenuItem } from '../../components/SideBar'
+
+const Menu = (
+  <GroupMenuItem title="Registros" icon="home" to="/home">
+    <MenuItem title="Ciudadanos" to="/home/dashboard-1" />
+    <MenuItem title="Estados" to="/home/dashboard-1" />
+    <MenuItem title="Municipios" to="/home/dashboard-1" />
+  </GroupMenuItem>
+)
+
+export default Menu
