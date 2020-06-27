f9168ed0d81029bc0bf9a49c1d3c138e99802bb1
Ricardo Angeles <Ricardo.ang.rmz@gmail.com>
2020-06-27 01:12:37 -0500

    Table_progress

--- src/js/routes/home/citizen_registration/Table_citizens.js
+++ src/js/routes/home/citizen_registration/Table_citizens.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
-import { Row, Col, Button, Clearfix } from 'react-bootstrap'
+import { Row, Col, Button, Clearfix,  } from 'react-bootstrap'
 
 class TableCitizens extends Component {
+
     render () {
         return(
-            <div className="dashboard_graph">
-
-            <div className="col-md-9 col-sm-9 col-xs-12">
-            <div id="chart_plot_01" className="demo-placeholder"></div>
-            </div>
-
+            <div className="Table_cit">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                        <th>Nombre</th>
+                        <th>Direccion</th>
+                        <th>Estado</th>
+                        <th>Municipio</th>
+                        <th>Telefono</th>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                    </thead>
+                    <tbody className="Table_cont">
+                        <FillTable />
+                    </tbody>
+                </table>
             <Clearfix />
-        </div>
+            </div>
         )
     }
+}
+
+function FillTable(props){
+    return(
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.direction}</td>
+            <td>{props.state}</td>
+            <td>{props.town}</td>
+            <td>{props.phone}</td>
+            <td><Button><i class="fa fa-trash-o fa-4" aria-hidden="true"></i></Button></td>
+            <td><Button><i class="fa fa-pencil-square-o fa-4" aria-hidden="true"></i></Button></td>
+        </tr>
+    )
 }
 
 export default TableCitizens
