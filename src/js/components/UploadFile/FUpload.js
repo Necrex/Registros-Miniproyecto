23bc0efbadc3d1c2881b51e9a29ae96f4b3cface
Ricardo Angeles <Ricardo.ang.rmz@gmail.com>
2020-06-27 22:08:27 -0500

    Form_citizens_changes

--- src/js/components/UploadFile/FUpload.js
+++ src/js/components/UploadFile/FUpload.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+import { Button, Row, Col } from 'react-bootstrap'
+
 
 class FUpload extends Component {
     constructor () {
@@ -30,8 +32,15 @@ class FUpload extends Component {
         return(
             <div>
                 {/* <img width="100" src={this.props.picture} /> */}
-                <img height="200" width="250" src="http://www.pequenomundo.cl/wp-content/themes/childcare/images/default.png" />
-                <input type="file" onChange={this.handleUpload} />
+                <img className="img-thumbnail" height="200" width="250" src="http://www.pequenomundo.cl/wp-content/themes/childcare/images/default.png" />                
+
+                <input className="custom-file-input" type="file" onChange={this.handleUpload} />
+                <br/>
+                <br/>
+                <div>
+                    <Button className="btn-success">Guardar</Button>
+                    <Button className="btn-danger">Limpiar</Button>
+                </div>
             </div>
         )
     }
--- src/js/routes/home/citizen_register/Citizen_form.js
+++ src/js/routes/home/citizen_register/Citizen_form.js
@@ -1,22 +1,94 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Clearfix } from 'react-bootstrap'
 import FUpload from '../../../components/UploadFile/FUpload.js'
 
 
 class CitizenReg extends Component {
   render () {
-
     return (
         <div>
             <Row>
-                <label>Registro de ciudadanos</label>
+                <Col md={8}>
+                    <label>Registro de ciudadanos</label>
+                </Col>
             </Row>
-            <Row>
-                
-            </Row>
+            <br></br>
+            <Clearfix/>
+            <form>
+                <Row>
+                    <Col md={2}>
+                    <label style={{fontSize:'13pt'}}>Nombre *</label>
+                    </Col>
+                    <Col md={5}>
+                    <input className="form-control" type="text"></input>
+                    </Col>
+                </Row>
+                <br></br>
+                <Clearfix/>
+                <Row>
+                    <Col md={2}>
+                    <label style={{fontSize:'13pt'}}>Direccion *</label>
+                    </Col>
+                    <Col md={5}>
+                    <input className="form-control" type="text"></input>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                    <Col md={2}>
+                    <label style={{fontSize:'13pt'}}>Estado *</label>
+                    </Col>
+                    <Col md={5}>
+                    <div class="input-group mb-3">
+                    <select class="form-control" id="inputGroupSelect01">
+                        <Callstates />
+                    </select>
+                    </div>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                    <Col md={2}>
+                    <label style={{fontSize:'13pt'}}>Municipio *</label>
+                    </Col>
+                    <Col md={5}>
+                    <div class="input-group mb-3">
+                    <select class="form-control" id="inputGroupSelect01">
+                        <Calltowns />
+                    </select>
+                    </div>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                    <Col md={2}>
+                    <label style={{fontSize:'13pt'}}>Telefono</label>
+                    </Col>
+                    <Col md={3}>
+                    <input className="form-control" type="text"></input>
+                    </Col>
+                </Row>
+            </form>
+            <div className="FUpload">
+                <FUpload/>
+            </div>
+            <br/>
         </div>
+        
     )
   }
+}
+
+function Callstates() {
+    return(
+        <option selected>Aguascalientes</option>
+    )
+}
+
+function Calltowns() {
+    return(
+        <option selected>Aguascalientes</option>
+    )
 }
 
 export default CitizenReg
